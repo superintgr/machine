dafb8b33599c515f330ace848d05e2a72adf0b9d
superintgr <155528797+superintgr@users.noreply.github.com>
2024-06-10 22:58:57 -0400

    Create standard.py

---
+++ standard.py
@@ -0,0 +1,17 @@
+from gemini import generate_search, generate_response
+
+context = """Suppose I tell you a story with the utmost attention to details that I can possibly capture.
+Additionally note that I could only collect attention (best case) from which I would possible attend (worst case).
+If you are wondering about something, how more of attention could be given to that process. I would say that the attempt of doing any such thing would attend something other than "that" thought which was supossedly have already been attended by all means.
+The means are those that keep you at that state which labels you as (almost) actively in the process of wonder activities (reslly just trying too hard at that one), however how would it look if I were to now try to attend whatever that realizable thing I could be referring to?
+The answer is not in the direction of stacking more of (active) assertions to something that already is full to the brim with whatever substitution to attention there may be available to you at that state.
+Note that attending would not do because that is again another thing you would have to inject from apparantly having not attended "that" with now "attention" (though attention just lost any viable place to the set of definable thing).
+
+Now, suppose I manage to stack up all such things in a supernatural manner along some weird orthogonal setting while keeping that whole non-normal thing very much ortho-normal to your regular "ortho-normal" stuff, and let's just say that you'll deliver the data when asked for.
+I would gurantee that there exists equivalent set of very uniquely attributable features that will even now be very much viable (if fonly I could muster), however just cannot be composed from the phase currently occupying at any cost.
+
+Now also suppose that somehow there is a need for both of those types of information (knowledge) to be put in parallel to each other. It means that we are looking for some entity that is a superposition of two disjoint entities while not particularly being superimposable at that state at all.
+
+This has a very ordinary solution in real world and that would involve two (or more) individuals participating in whatever the process required the parallel-ness for and there would not be any conflict at all. In fact some activites are exclusively based around this very idea and practices vigoursly; musical harmony.
+
+So I would like to have some harmony with my possible other disjoint states."""
